259d7e06fb3707f727924f544584177788adb77f
hongorgerelchimeg <g.hongoroo@gmail.com>
2022-03-02 00:02:13 +0100

    problem solved, exercise complete.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+// Esercizio 1
+
 const btnCheck = document.querySelector('#check');
 
 
@@ -5,7 +7,7 @@ btnCheck.addEventListener('click', function() {
 
     const input = document.querySelector('#user-input');
     const output = document.querySelector('.output');
-    userValue = input.value ;
+    let userValue = input.value ;
     console.log(userValue);
 
     let reverseInput = '';
@@ -25,9 +27,9 @@ btnCheck.addEventListener('click', function() {
 })
 
 
-// function palidroma(userValue) {
+// function palidroma(a) {
 //     let reverseInput;
-//     for (let index = userValue.length; index > 0 ; index--) {
+//     for (let index = a.length; index > 0 ; index--) {
         
 //         reverseInput += userValue[index];
 //     }
@@ -40,5 +42,43 @@ btnCheck.addEventListener('click', function() {
     
 // }
 
+// Esercizio 2
+
+const btnStart = document.querySelector('#start');
 
 
+btnStart.addEventListener('click', function() {
+
+    const inputNumber = document.querySelector('#user-input-3');
+    const inputFinal = document.querySelector('#user-input-2');
+    const outputGioco = document.querySelector('.output-gioco');
+    let userNumber = parseInt(inputNumber.value) ;
+    let userFinal = inputFinal.value ;
+    console.log(userNumber);
+    console.log(userFinal);
+
+    let computerNumber = randomNum(1, 5);
+    console.log(computerNumber);
+
+    const result = pariDispari(userNumber, computerNumber);
+
+    if ( result == userFinal) {
+        return outputGioco.innerHTML = `computer ha scelto: ${computerNumber}, tu hai scelto: ${userNumber} , somma  è ${userFinal}, hai vinto!`;
+    } else {
+         return outputGioco.innerHTML = `computer ha scelto: ${computerNumber}, tu hai scelto: ${userNumber} , somma non è ${userFinal}, hai perso!`;
+    }
+
+});
+
+function randomNum(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+  }
+
+function pariDispari(a, b) {
+    console.log(a, b);
+    if ((a + b) % 2 == 0) {
+        return "Pari";
+    } else {
+        return "Dispari";
+    }
+}
